0a0397e612bdc4d38546a570dd5afe56ebeb4714
David Pacheco <davidpch@gmail.com>
2020-03-15 09:23:55 +0000

    version change to match original upstream

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import warnings
 
 from setuptools import setup, find_packages
 
-version = '0.10.3.1'
+version = '0.10.2.1'
 
 def parse_requirements(filename):
     """ load requirements from a pip requirements file """
